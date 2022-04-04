e41a81decbdfacf91fcc2b22b16a667f96a5bde9
Milena <milenka.dimova@gmail.com>
2022-04-04 13:28:23 +0300

    Implemented some logic in the ControllerImpl.java

--- 18 April 2021/SpaceStation/src/main/java/spaceStation/core/ControllerImpl.java
+++ 18 April 2021/SpaceStation/src/main/java/spaceStation/core/ControllerImpl.java
@@ -1,20 +1,69 @@
 package spaceStation.core;
 
+import spaceStation.common.ConstantMessages;
+import spaceStation.common.ExceptionMessages;
+import spaceStation.models.astronauts.Astronaut;
+import spaceStation.models.astronauts.Biologist;
+import spaceStation.models.astronauts.Geodesist;
+import spaceStation.models.astronauts.Meteorologist;
+import spaceStation.models.planets.PlanetImpl;
+import spaceStation.repositories.AstronautRepository;
+import spaceStation.repositories.PlanetRepository;
+
+import java.util.Arrays;
+
 public class ControllerImpl implements Controller {
+    private AstronautRepository astronautRepository;
+    private PlanetRepository planetRepository;
+
+    public ControllerImpl() {
+        this.astronautRepository = new AstronautRepository();
+        this.planetRepository = new PlanetRepository();
+    }
 
     @Override
     public String addAstronaut(String type, String astronautName) {
-        return null;
+        Astronaut astronaut;
+        switch (type) {
+            case "Biologist" :
+                astronaut = new Biologist(astronautName);
+                break;
+            case "Geodesist":
+                astronaut = new Geodesist(astronautName);
+                break;
+            case "Meteorologist":
+                astronaut = new Meteorologist(astronautName);
+                break;
+            default:
+                throw new IllegalArgumentException(ExceptionMessages.ASTRONAUT_INVALID_TYPE);
+        }
+
+        this.astronautRepository.add(astronaut);
+
+        return String.format(ConstantMessages.ASTRONAUT_ADDED, type, astronautName);
     }
 
     @Override
     public String addPlanet(String planetName, String... items) {
-        return null;
+        PlanetImpl planet = new PlanetImpl(planetName);
+        planet.setItems(Arrays.asList(items));
+        this.planetRepository.add(planet);
+
+        return String.format(ConstantMessages.PLANET_ADDED, planetName);
     }
 
     @Override
     public String retireAstronaut(String astronautName) {
-        return null;
+        Astronaut astronaut = this.astronautRepository.findByName(astronautName);
+
+        if (astronaut == null) {
+            throw new IllegalArgumentException(
+                    String.format(ExceptionMessages.ASTRONAUT_DOES_NOT_EXIST, astronautName));
+        }
+
+        this.astronautRepository.remove(astronaut);
+
+        return String.format(ConstantMessages.ASTRONAUT_RETIRED, astronautName);
     }
 
     @Override
