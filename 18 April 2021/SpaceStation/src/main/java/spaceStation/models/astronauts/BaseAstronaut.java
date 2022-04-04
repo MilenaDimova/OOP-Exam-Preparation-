c2417cdca252851576e17b9c81db89b6ba3a8a89
Milena <milenka.dimova@gmail.com>
2022-04-04 11:45:41 +0300

    create baseAstronaut class

---
+++ 18 April 2021/SpaceStation/src/main/java/spaceStation/models/astronauts/BaseAstronaut.java
@@ -0,0 +1,55 @@
+package spaceStation.models.astronauts;
+
+import spaceStation.common.ExceptionMessages;
+import spaceStation.models.bags.Bag;
+
+public abstract class BaseAstronaut implements Astronaut{
+    private String name;
+    private double oxygen;
+
+    private static final int DECREASE_OXYGEN = 10;
+
+    protected BaseAstronaut(String name, double oxygen) {
+        this.setName(name);
+        this.setOxygen(oxygen);
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(ExceptionMessages.ASTRONAUT_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    protected void setOxygen(double oxygen) {
+        if (oxygen < 0) {
+            throw new IllegalArgumentException(ExceptionMessages.ASTRONAUT_OXYGEN_LESS_THAN_ZERO);
+        }
+        this.oxygen = oxygen;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public double getOxygen() {
+        return this.oxygen;
+    }
+
+    @Override
+    public boolean canBreath() {
+        return false;
+    }
+
+    @Override
+    public Bag getBag() {
+        return null;
+    }
+
+    @Override
+    public void breath() {
+        this.setOxygen(this.getOxygen() - DECREASE_OXYGEN);
+    }
+}
