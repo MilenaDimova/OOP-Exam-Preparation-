874b8a0e62f9b39733e2641453d9203c4fa46c5b
Milena <milenka.dimova@gmail.com>
2022-04-04 16:30:04 +0300

    add method breath

--- 18 April 2021/SpaceStation/src/main/java/spaceStation/models/mission/MissionImpl.java
+++ 18 April 2021/SpaceStation/src/main/java/spaceStation/models/mission/MissionImpl.java
@@ -4,6 +4,7 @@ import spaceStation.models.astronauts.Astronaut;
 import spaceStation.models.bags.Bag;
 import spaceStation.models.planets.Planet;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class MissionImpl implements Mission{
@@ -12,12 +13,14 @@ public class MissionImpl implements Mission{
     public void explore(Planet planet, Collection<Astronaut> astronauts) {
         for (Astronaut astronaut : astronauts) {
             Bag bag = astronaut.getBag();
-            for (String item : planet.getItems()) {
+            Collection<String> items = new ArrayList<>(planet.getItems());
+            for (String item : items) {
                 if (!astronaut.canBreath()) {
                     break;
                 }
                 bag.getItems().add(item);
                 planet.getItems().remove(item);
+                astronaut.breath();
             }
         }
     }
