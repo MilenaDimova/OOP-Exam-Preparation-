886d2b81001f40ac636c86b6579e40373d2d8fe2
Milena <milenka.dimova@gmail.com>
2022-04-04 12:51:57 +0300

    create PlanetRepository

---
+++ 18 April 2021/SpaceStation/src/main/java/spaceStation/repositories/PlanetRepository.java
@@ -0,0 +1,30 @@
+package spaceStation.repositories;
+
+import spaceStation.models.planets.Planet;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class PlanetRepository implements Repository<Planet> {
+    private Collection<Planet> planets;
+
+    @Override
+    public Collection<Planet> getModels() {
+        return Collections.unmodifiableCollection(planets);
+    }
+
+    @Override
+    public void add(Planet planet) {
+        this.planets.add(planet);
+    }
+
+    @Override
+    public boolean remove(Planet planet) {
+        return this.planets.remove(planet);
+    }
+
+    @Override
+    public Planet findByName(String name) {
+        return this.planets.stream().filter(p -> p.getName().equals(name)).findFirst().get();
+    }
+}
