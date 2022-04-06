6aad4b3c18806da75180c77ac0aa74464306c69d
Milena <milenka.dimova@gmail.com>
2022-04-06 19:03:38 +0300

    Implements RandomAccessMemory.java

---
+++ 16 August 2020/OnlineShop/src/onlineShop/models/products/components/RandomAccessMemory.java
@@ -0,0 +1,15 @@
+package onlineShop.models.products.components;
+
+public class RandomAccessMemory extends BaseComponent {
+    private static final double DEFAULT_UNIT_MULTIPLIER = 1.20;
+
+    public RandomAccessMemory(int id, String manufacturer, String model, double price,
+                       double overallPerformance, int generation) {
+        super(id, manufacturer, model, price, overallPerformance, generation);
+    }
+
+    @Override
+    public void setOverallPerformance(double overallPerformance) {
+        super.setOverallPerformance(this.getOverallPerformance() * DEFAULT_UNIT_MULTIPLIER);
+    }
+}
