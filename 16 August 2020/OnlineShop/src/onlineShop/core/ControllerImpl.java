94e9aa1c664ada9270e5d26cae5189695207054d
Milena <milenka.dimova@gmail.com>
2022-04-07 11:36:30 +0300

    Changes in removePeripheral(). Changes in removeComponent(). Implemented buyComputer(). Implemented buyBestComputer(). Refactor getComputerById().

--- 16 August 2020/OnlineShop/src/onlineShop/core/ControllerImpl.java
+++ 16 August 2020/OnlineShop/src/onlineShop/core/ControllerImpl.java
@@ -8,8 +8,8 @@ import onlineShop.models.products.computers.Laptop;
 import onlineShop.models.products.peripherals.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 import static onlineShop.common.constants.ExceptionMessages.*;
 import static onlineShop.common.constants.OutputMessages.*;
@@ -45,7 +45,7 @@ public class ControllerImpl implements Controller {
     @Override
     public String addPeripheral(int computerId, int id, String peripheralType, String manufacturer, String model,
                                 double price, double overallPerformance, String connectionType) {
-        Peripheral peripheral = null;
+        Peripheral peripheral;
         switch (peripheralType) {
             case "Headset":
                 peripheral = new Headset(id, manufacturer, model, price, overallPerformance, connectionType);
@@ -76,15 +76,7 @@ public class ControllerImpl implements Controller {
     @Override
     public String removePeripheral(String peripheralType, int computerId) {
         Computer computer = getComputerById(computerId);
-
-        Peripheral peripheral = computer.getPeripherals().stream()
-                .filter(p -> p.getClass().getSimpleName().equals(peripheralType))
-                .findFirst()
-                .orElse(null);
-
-        if (peripheral != null) {
-            computer.getComponents().remove(peripheral);
-        }
+        Peripheral peripheral = computer.removePeripheral(peripheralType);
 
         return String.format(REMOVED_PERIPHERAL, peripheralType, peripheral.getId());
     }
@@ -92,7 +84,7 @@ public class ControllerImpl implements Controller {
     @Override
     public String addComponent(int computerId, int id, String componentType, String manufacturer,
                                String model, double price, double overallPerformance, int generation) {
-        Component component = null;
+        Component component;
         switch (componentType) {
             case "CentralProcessingUnit":
                 component = new CentralProcessingUnit(id, manufacturer, model, price, overallPerformance, generation);
@@ -106,7 +98,8 @@ public class ControllerImpl implements Controller {
             case "RandomAccessMemory":
                 component = new RandomAccessMemory(id, manufacturer, model, price, overallPerformance, generation);
                 break;
-            case "SolidStateDrive": new SolidStateDrive(id, manufacturer, model, price, overallPerformance, generation);
+            case "SolidStateDrive":
+                component = new SolidStateDrive(id, manufacturer, model, price, overallPerformance, generation);
                 break;
             case "VideoCard":
                 component = new VideoCard(id, manufacturer, model, price, overallPerformance, generation);
@@ -119,6 +112,7 @@ public class ControllerImpl implements Controller {
         if (computer.getComponents().stream().anyMatch(c -> c.getId() == id)) {
             throw new IllegalArgumentException(EXISTING_COMPONENT_ID);
         }
+
         computer.addComponent(component);
 
         return String.format(ADDED_COMPONENT, componentType, id, computer.getId());
@@ -127,27 +121,29 @@ public class ControllerImpl implements Controller {
     @Override
     public String removeComponent(String componentType, int computerId) {
         Computer computer = getComputerById(computerId);
-
-        Component component = computer.getComponents().stream()
-                .filter(c -> c.getClass().getSimpleName().equals(componentType))
-                .findFirst()
-                .orElse(null);
-
-        if (component != null) {
-            computer.getComponents().remove(component);
-        }
+        Component component = computer.removeComponent(componentType);
 
         return String.format(REMOVED_COMPONENT, componentType, component.getId());
     }
 
     @Override
     public String buyComputer(int id) {
-        return null;
+        Computer computer = getComputerById(id);
+        this.computers.remove(computer);
+
+        return computer.toString();
     }
 
     @Override
     public String BuyBestComputer(double budget) {
-        return null;
+        Computer computer = this.computers.stream()
+                .filter(c -> c.getPrice() <= budget)
+                .max(Comparator.comparing(Computer::getOverallPerformance))
+                .orElseThrow(() -> new IllegalArgumentException(String.format(CAN_NOT_BUY_COMPUTER, budget)));
+
+        this.buyComputer(computer.getId());
+
+        return computer.toString();
     }
 
     @Override
@@ -156,11 +152,7 @@ public class ControllerImpl implements Controller {
     }
 
     private Computer getComputerById(int id) {
-        Optional<Computer> computer = this.computers.stream().filter(c -> c.getId() == id).findAny();
-        if (computer.isEmpty()) {
-            throw new IllegalArgumentException(EXISTING_COMPUTER_ID);
-        }
-
-        return computer.get();
+        return this.computers.stream().filter(c -> c.getId() == id).findAny()
+                .orElseThrow(() -> new IllegalArgumentException(EXISTING_COMPUTER_ID));
     }
 }
