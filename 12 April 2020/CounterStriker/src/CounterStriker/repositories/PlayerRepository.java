6751ce08ffe4a2193f48503f667638bbaab11c0b
Milena <milenka.dimova@gmail.com>
2022-04-07 20:28:50 +0300

    Implements PlayerRepository

--- 12 April 2020/CounterStriker/src/CounterStriker/repositories/PlayerRepository.java
+++ 12 April 2020/CounterStriker/src/CounterStriker/repositories/PlayerRepository.java
@@ -2,28 +2,39 @@ package CounterStriker.repositories;
 
 import CounterStriker.models.players.Player;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+
+import static CounterStriker.common.ExceptionMessages.INVALID_PLAYER_REPOSITORY;
 
 public class PlayerRepository implements Repository<Player> {
     private Collection<Player> models;
 
+    public PlayerRepository() {
+        this.models = new ArrayList<>();
+    }
+
     @Override
     public Collection<Player> getModels() {
-        return null;
+        return Collections.unmodifiableCollection(this.models);
     }
 
     @Override
     public void add(Player model) {
-
+        if (model == null) {
+            throw new NullPointerException(INVALID_PLAYER_REPOSITORY);
+        }
+        this.models.add(model);
     }
 
     @Override
     public boolean remove(Player model) {
-        return false;
+        return this.models.remove(model);
     }
 
     @Override
     public Player findByName(String name) {
-        return null;
+        return this.models.stream().filter(m -> m.getUsername().equals(name)).findFirst().orElse(null);
     }
 }
