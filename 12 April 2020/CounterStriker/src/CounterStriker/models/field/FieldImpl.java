a8fd45316a37487627e8a3d69833a60fd371454b
Milena <milenka.dimova@gmail.com>
2022-04-08 13:50:49 +0300

    Implements FieldImpl

--- 12 April 2020/CounterStriker/src/CounterStriker/models/field/FieldImpl.java
+++ 12 April 2020/CounterStriker/src/CounterStriker/models/field/FieldImpl.java
@@ -1,13 +1,51 @@
 package CounterStriker.models.field;
 
+import CounterStriker.models.guns.Gun;
 import CounterStriker.models.players.Player;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static CounterStriker.common.OutputMessages.*;
 
 public class FieldImpl implements Field{
 
     @Override
     public String start(Collection<Player> players) {
-        return null;
+        List<Player> terrorists = players.stream()
+                .filter(p -> p.getClass().getSimpleName().equals("Terrorist"))
+                .collect(Collectors.toList());
+
+        List<Player> counterTerrorists = players.stream()
+                .filter(p -> p.getClass().getSimpleName().equals("CounterTerrorist"))
+                .collect(Collectors.toList());
+
+        while (terrorists.stream().allMatch(Player::isAlive) || counterTerrorists.stream().allMatch(Player::isAlive)) {
+            for (Player terrorist : terrorists) {
+                for (Player counterTerrorist : counterTerrorists) {
+                    terrorist.getGun().fire();
+                    int damage = terrorist.getGun().fire();
+                    counterTerrorist.takeDamage(damage);
+                }
+            }
+
+            for (Player counterTerrorist : counterTerrorists) {
+                for (Player terrorist : terrorists) {
+                    counterTerrorist.getGun().fire();
+                    int damage = counterTerrorist.getGun().fire();
+                    terrorist.takeDamage(damage);
+                }
+            }
+        }
+
+        String result = "";
+        if (counterTerrorists.stream().noneMatch(Player::isAlive)) {
+            result = TERRORIST_WINS;
+        } else {
+            result = COUNTER_TERRORIST_WINS;
+        }
+
+        return result;
     }
 }
